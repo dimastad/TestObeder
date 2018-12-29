fea50250822801e8050af07cc3e9664613692a51
Dmitrii Portnov <dimastad29@gmail.com>
2018-12-29 13:11:36 +0400

    Updates

---
+++ precache-manifest.f9b3264240ace2b610849d7bd91d4f86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f592a9d4c000bf71993aafeaac22bce5",
+    "url": "/TestObeder/static/media/logo.f592a9d4.svg"
+  },
+  {
+    "revision": "55de0a52431fc12e9460",
+    "url": "/TestObeder/static/js/runtime~main.55de0a52.js"
+  },
+  {
+    "revision": "5e68c1ad13a733931fb7",
+    "url": "/TestObeder/static/js/main.5e68c1ad.chunk.js"
+  },
+  {
+    "revision": "0b75dba2ef791fb234c5",
+    "url": "/TestObeder/static/js/1.0b75dba2.chunk.js"
+  },
+  {
+    "revision": "5e68c1ad13a733931fb7",
+    "url": "/TestObeder/static/css/main.f0f07b7c.chunk.css"
+  },
+  {
+    "revision": "0b75dba2ef791fb234c5",
+    "url": "/TestObeder/static/css/1.440d798f.chunk.css"
+  },
+  {
+    "revision": "09a39307b189ba650acd5212c4570e35",
+    "url": "/TestObeder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/TestObeder/precache-manifest.cd546a2a5abc1d36e91d27357c708922.js"
+  "/TestObeder/precache-manifest.f9b3264240ace2b610849d7bd91d4f86.js"
 );
 
 workbox.clientsClaim();
