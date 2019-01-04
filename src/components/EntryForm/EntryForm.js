7bd5994809a7d5419f147bc46c5ef517038ad180
Dmitrii Portnov <dimastad29@gmail.com>
2019-01-05 02:32:40 +0400

    logo-title size

--- src/components/EntryForm/EntryForm.js
+++ src/components/EntryForm/EntryForm.js
@@ -9,7 +9,7 @@ export default class EntryForm extends Component {
             <div className="EntryFormWrapper" hidden>
                 <form className="form-signin">
                     <img src={logo} className="mb-3" alt="logo" width="160" height="160" />
-                    <h1 className="h1 logo-title mb-5">obeder</h1>
+                    <h1 className="logo-title logo-title_mobile-form mb-5">obeder</h1>
                     <input type="email" class="inputForm mb-3" placeholder="Электропочта" required ></input>
                     <input type="password" class="inputForm mb-5" placeholder="Кодовое слово" required ></input>
                     <Button type="submit" buttonText="Обедать!" />
