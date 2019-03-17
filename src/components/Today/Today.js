c87fe02f241a30e1ebd8e5942e053ee9ee0fcbf3
Dmitrii Portnov <dimastad29@gmail.com>
2019-03-17 12:47:51 +0400

    0.fix bug

--- src/components/Today/Today.js
+++ src/components/Today/Today.js
@@ -28,7 +28,7 @@ export default class Today extends Component {
                     <Menu theClass="menu span-3" menuheader="Гарниры" id1="reeze" dish1="Рис-хуис" id2="paste" dish2="Макарохи" />
                     <Menu theClass="menu span-4" menuheader="Вторые блюда" id1="karbonara" dish1="Уберкарбонара с соусом из усов шанхайского барса" id2="ravioly" dish2="Пельмени зачотные" lock="disabled"/>
                     <div className="span-4">
-                        <div className="restangle"></div>
+                        <textarea className="restangle"></textarea>
                         <Button type="reset" buttonText="Сбросить в дефолт!" />
                     </div>
                     
